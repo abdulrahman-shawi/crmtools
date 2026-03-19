30fdc97d44571e07784540b36b198a7e2b942f4e
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-03-20 00:42:03 +0300

    قق

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate"
+          }
+        ]
+      },
+      {
+        source: "/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          }
+        ]
+      },
+      {
+        source: "/_next/image(|s)?/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=60, must-revalidate"
+          }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
